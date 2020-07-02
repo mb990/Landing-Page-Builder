758dffe9296a37f5b8f396059aaf95fae00e4e05
pavle.birac <pavle.birac@quantox.com>
2020-07-02 11:48:46 +0200

    Homepage test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,62 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="app.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script type='text/javascript' src="app.js"></script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <a class="js-back-to-top" id="button"></a>
+        <header class="header-main">
+            <div style="flex-grow: 1;"><img style="width: 100px;transform: translateY(-25%);" src="/logo.png"></div>
+            <div class="js-link">
+                <button class="head-link" id="js-about">About</button>
+                <button class="head-link" id="js-info">Info</button>
+            </div>
+        </header>
+        <main class="main-content">
+            <div>
+                <h1>Your future
+                    <span class="main-word1">landing page is here</span>
+                </h1>
+                <div style="text-align: center;">
+                    <button class="create-btn">Create landing page</button>
                 </div>
             </div>
-        </div>
+        </main>
+        <main class="main-content2 link-js-about">
+            <div class="text2">
+                <h3>What is this about?</h3>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis ipsum erat, 
+                et porta ipsum auctor eget. Cras euismod tortor eget commodo sagittis. 
+                Pellentesque ut tor egestas est. Praesent urna 
+                risus, dictum eget euismod et, fere cursus aliquam 
+                sodales. Quisque dignissim est ut diam euismod, ornare tempus risus vulputate
+            </div>
+            <div style="width: fill-available;">
+                <img style="width:90%; height:90%;" class="star" src="/code.jpg">
+            </div>
+        </main>
+        <main class="main-content2 link-js-info">
+            <div style="width: fill-available;">
+                <img style="width:90%; height:90%;" class="star" src="/people.jpg">
+            </div>
+            <div class="text2">
+                <h3>More about us?</h3>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed pretium diam. Quisque feugiat ac nisi a molestie. 
+                Praesent sed urna suscipit, volutpat magna vel, condimentum arcu. Maecenas euismod eu metus sed sollicitudin. Nunc sit 
+                amet bibendum orci. In magna nunc, faucibus quis interdum rutrum, pretiuma in volutpat. 
+                Etiam congue id urna id suscipit. Proin pellentesque risus a neque facilisis placerat. Aliquam at finibus eros.
+            </div>
+        </main>
+        <footer class="footer-main">
+            Copyright 2020 © Future Web
+        </footer>
+        <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     </body>
 </html>
