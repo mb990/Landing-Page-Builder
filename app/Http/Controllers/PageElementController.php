5081b91cd2be51021ef8e6f4e3c0481ccac82f08
spasoje.gojic <spasoje.gojic@quantox.com>
2020-07-06 15:38:34 +0200

    Added saving methods for page elements and top menu links

--- app/Http/Controllers/PageElementController.php
+++ app/Http/Controllers/PageElementController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\PageElementService;
 use Illuminate\Http\Request;
 
 class PageElementController extends Controller
 {
-    //
+    /**
+     * @var PageElementService
+     */
+    private $pageElementService;
+
+    public function __construct(PageElementService $pageElementService)
+    {
+        $this->pageElementService = $pageElementService;
+    }
+
+    public function store(Request $request)
+    {
+        $element = $this->pageElementService->store($request);
+
+        return response()->json(['element' => $element, 'success' => 'Page element is created']);
+    }
 }
--- app/Http/Controllers/TopMenuLinkController.php
+++ app/Http/Controllers/TopMenuLinkController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TopMenuLinkService;
 use Illuminate\Http\Request;
 
 class TopMenuLinkController extends Controller
 {
-    //
+    /**
+     * @var TopMenuLinkService
+     */
+    private $topMenuLinkService;
+
+    public function __construct(TopMenuLinkService $topMenuLinkService)
+    {
+        $this->topMenuLinkService = $topMenuLinkService;
+    }
+
+    public function store(Request $request)
+    {
+        $link = $this->topMenuLinkService->store($request);
+
+        return response()->json(['link' => $link, 'success' => 'New link is stored']);
+    }
 }
