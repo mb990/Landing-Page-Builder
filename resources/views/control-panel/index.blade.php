5c26977aeb172935b83b13cc49fd3b2e51d87d73
pavle.birac <pavle.birac@quantox.com>
2020-07-06 11:57:49 +0200

    Admin panel

--- resources/views/control-panel/index.blade.php
+++ resources/views/control-panel/index.blade.php
@@ -1,1 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    </head>
+    <body>
+        <div style="width: 50vw;margin-top: 100px;">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
+                <div class="input-group-prepend">
+                    <button class="btn btn-outline-secondary" type="button">Button</button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
