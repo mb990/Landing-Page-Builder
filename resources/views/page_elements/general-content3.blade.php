b00a3e879b5deaee4d7e977f4504cfa92b3d4ebb
pavle.birac <pavle.birac@quantox.com>
2020-07-09 12:59:14 +0200

    General content bullets added v1.1

--- resources/views/page_elements/general-content3.blade.php
+++ resources/views/page_elements/general-content3.blade.php
@@ -11,10 +11,26 @@
                                 Maecenas molestie dui id diam feugiat, eu tincidunt mauris aliquam. Duis commodo vitae ligula et int
                                 Aliquam nec dignissim nisl. Vivamus cursus feugiat sapien, eget tincidunt leo ornare quis.</p>
                         </div>
-                        <a class="btn btn-lg btn-filled" href="#">MORE INFO</a>
+                        <a class="btn btn-lg btn-filled btn-secondary" href="#">MORE INFO</a>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+    <div style=" padding: 20vh;">
+        <div class="content center" style="display: flex;">
+            <div class="col span1of3 mid-span3of4 small-span1of1 align-left">
+                <h2 class="heading center">Find your people</h2>
+                <p>Personalize your signup form and share it with your community. Send a link to your landing page, embed the form on your site, or invite readers to subscribe right on Twitter. Send great emails, and watch your readership grow over time.</p>
+            </div>
+            <div class="col span1of3 mid-span3of4 small-span1of1 align-left">
+                <h2 class="heading center">Share your work</h2>
+                <p>Your readers care what you have to say. Email them updates from your world. Give them behind-the-scenes peeks at your projects and process, or show them your published work. Point them to things you’re reading, watching, and listening to.</p>
+            </div>
+            <div class="col span1of3 mid-span3of4 small-span1of1 align-left">
+                <h2 class="heading center">Keep it simple</h2>
+                <p>TinyLetter offers a clean and straightforward writing experience for people who aren’t looking for advanced reporting or features for businesses. Use our editor or send directly from your own email client. And all for free.</p>
+            </div>
+        </div>
+    </div>
 </div>
