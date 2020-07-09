b47b493cb3e7580f3c673d8a290a41a5fd2a990c
pavle.birac <pavle.birac@quantox.com>
2020-07-09 16:35:39 +0200

    Mobile view template updated, style update 1.3

--- resources/views/page_elements/general-content3.blade.php
+++ resources/views/page_elements/general-content3.blade.php
@@ -19,18 +19,7 @@
     </section>
     <div style=" padding: 20vh;">
         <div class="content center" style="display: flex;">
-            <div class="col span1of3 mid-span3of4 small-span1of1 align-left">
-                <h2 class="heading center">Find your people</h2>
-                <p>Personalize your signup form and share it with your community. Send a link to your landing page, embed the form on your site, or invite readers to subscribe right on Twitter. Send great emails, and watch your readership grow over time.</p>
-            </div>
-            <div class="col span1of3 mid-span3of4 small-span1of1 align-left">
-                <h2 class="heading center">Share your work</h2>
-                <p>Your readers care what you have to say. Email them updates from your world. Give them behind-the-scenes peeks at your projects and process, or show them your published work. Point them to things you’re reading, watching, and listening to.</p>
-            </div>
-            <div class="col span1of3 mid-span3of4 small-span1of1 align-left">
-                <h2 class="heading center">Keep it simple</h2>
-                <p>TinyLetter offers a clean and straightforward writing experience for people who aren’t looking for advanced reporting or features for businesses. Use our editor or send directly from your own email client. And all for free.</p>
-            </div>
+            @include('general-content3-bullet.blade.php')
         </div>
     </div>
 </div>
