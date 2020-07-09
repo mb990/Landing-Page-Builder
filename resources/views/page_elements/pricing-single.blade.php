9cd9b13be350cc0818555d0a0902e3b34add1244
pavle.birac <pavle.birac@quantox.com>
2020-07-09 08:41:59 +0200

    Pricing element, sub-element  updated

---
+++ resources/views/page_elements/pricing-single.blade.php
@@ -0,0 +1,17 @@
+<div class="col-lg-4">
+    <div class="card mb-5 mb-lg-0">
+        <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Free</h5>
+            <h6 class="card-price text-center price-tag js-month">$0<span class="period">/month</span></h6>
+            <h6 class="card-price text-center price-tag d-none js-year">$10<span class="period">/year</span></h6>
+            <hr>
+            <ul class="fa-ul">
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>One Project</li>
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+            </ul>
+            <a href="#" class="btn btn-block btn-primary btn-lg">Get Plan</a>
+        </div>
+    </div>
+</div>
