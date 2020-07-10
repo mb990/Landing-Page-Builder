fcd106c26fd822559d612a499a844e37474523f8
spasoje.gojic <spasoje.gojic@quantox.com>
2020-07-10 15:54:17 +0200

    Removed social networks text on footer if no links are provided

--- resources/views/templates/template1/page_elements/footer.blade.php
+++ resources/views/templates/template1/page_elements/footer.blade.php
@@ -4,20 +4,25 @@
                 <div class="card-body">{{$element->creator}}</div>
                 <div>
                     <div class="card-body">
-                        <h4 class="card-title">Visit us on social media</h4>
+                        @if($element->facebook_url || $element->twitter_url || $element->instagram_url)
+
+                            <h4 class="card-title">Visit us on social media</h4>
+
                         <div class="template-demo">
                             @if($element->facebook_url)
                                 <a href="//{{$element->facebook_url}}" type="button" class="btn btn-social-icon btn-rounded"><i class="fa fa-facebook"></i></a>
                             @endif
 
                             @if($element->twitter_url)
-                                    <a href="//{{$element->twitter_url}}" type="button" class="btn btn-social-icon btn-rounded"><i class="fa fa-twitter"></i></a>
+                                <a href="//{{$element->twitter_url}}" type="button" class="btn btn-social-icon btn-rounded"><i class="fa fa-twitter"></i></a>
                             @endif
 
                             @if($element->instagram_url)
-                                    <a href="//{{$element->instagram_url}}" type="button" class="btn btn-social-icon btn-rounded"><i class="fa fa-instagram"></i></a>
+                                <a href="//{{$element->instagram_url}}" type="button" class="btn btn-social-icon btn-rounded"><i class="fa fa-instagram"></i></a>
                             @endif
                         </div>
+
+                        @endif
                     </div>
                 </div>
         </div>
