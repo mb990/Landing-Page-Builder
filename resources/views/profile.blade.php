938d46ebaa85df05fb52ae0a4afa002de4fc9e57
pavle.birac <pavle.birac@quantox.com>
2020-07-15 15:24:22 +0200

    Update profile v2.1

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/master.css') }}">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+        <header class="header-main-profile">
+            <div style="flex-grow: 1;"><img style="width: 100px;transform: translateY(-25%);" src="{{ asset('img/logo.png') }}"></div>
+            <div style="flex-grow: 1;">
+                <button class="js-mobile">Mobile</button>
+                <button class="js-desktop">Desktop</button>
+            </div>
+            <div class="js-link">
+                <button class="head-link" id="js-info">Log out</button>
+            </div>
+        </header>
+        <main style="display: flex;">
+            <aside class="profile-aside">
+                <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">New project</a>
+                    <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+                    <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
+                    <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">All project</a>
+                </div>
+            </aside>
+            <div class="profile-main js-profile-main">
+                <div class="tab-content" id="v-pills-tabContent">
+                    <!-- TAB1 -->
+                    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                        <div class="card-columns">
+                            <div class="card">
+                                <img class="card-img-top" src="https://source.unsplash.com/2gYsZUmockw/100px160/" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Template1</h5>
+                                    <a href="#" class="btn btn-primary">Use this template</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- TAB2 -->
+                    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+                    <!-- TAB3 -->
+                    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+                    <!-- TAB4 -->
+                    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+                        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                            <span>YOUR PROJECTS</span>
+                            <div class="card-columns">
+                                <div class="card">
+                                    <img class="card-img-top" src="https://source.unsplash.com/2gYsZUmockw/100px160/" alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Template1</h5>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <script>
+            $(".js-mobile").click(function() {
+                $(".js-profile-main").css('width','425px');
+            });
+            $(".js-desktop").click(function() {
+                $(".js-profile-main").css('width','100vw');
+            });
+        </script>
+    </body>
+</html>
--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -1,72 +1,90 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ asset('css/master.css') }}">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/page_elements1.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/page_elements2.css') }}">
+    <link rel="stylesheet" href="{{ asset('js/example.js') }}">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 
-    </head>
-    <body>
-        <header class="header-main-profile">
-            <div style="flex-grow: 1;"><img style="width: 100px;transform: translateY(-25%);" src="{{ asset('img/logo.png') }}"></div>
-            <div style="flex-grow: 1;">
-                <button class="js-mobile">Mobile</button>
-                <button class="js-desktop">Desktop</button>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+</head>
+<body class="body-t2">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-shrink" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger text-primary" href="#page-top">LOGO NAME</a>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger text-white bg-dark" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger text-white bg-dark" href="#projects">Projects</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger text-white bg-dark" href="#signup">Contact</a></li>
+                </ul>
             </div>
-            <div class="js-link">
-                <button class="head-link" id="js-info">Log out</button>
-            </div>
-        </header>
-        <main>
-            <aside class="profile-aside">
-                <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                    <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Overview</a>
-                    <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-                    <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-                    <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">New project</a>
-                </div>
-            </aside>
-            <div class="profile-main js-profile-main">
-                <div class="tab-content" id="v-pills-tabContent">
-                    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-                        <div class="card-columns">
-                            <div class="card">
-                                <img class="card-img-top" src="https://source.unsplash.com/2gYsZUmockw/100px160/" alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Template1</h5>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>                                </div>
-                            </div>
-                            <div class="card">
-                                <img class="card-img-top" src="https://source.unsplash.com/EMSDtjVHdQ8/100px160/" alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Template2</h5>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>                                </div>
-                            </div>
-                            <div class="card">
-                                <img class="card-img-top" src="https://source.unsplash.com/G9Rfc1qccH4/100px160/" alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Template3</h5>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>                                </div>
-                            </div>
-                        </div>
+        </div>
+    </nav>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="d-block w-100 hero-image-t2">
+                    <img class="hero-background" src="/img/bgd1.jpg">
+                    <div class="hero-text-t2 text-white">
+                        <h1>Travel to Belgrade</h1>
+                        <button class="btn btn-primary">Book weekend</button>
                     </div>
-                    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-                    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-                    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">Ovde ide novi pojekat</div>
                 </div>
             </div>
-        </main>
-        <script>
-            $(".js-mobile").click(function() {
-                $(".js-profile-main").css('width','425px');
-            });
-            $(".js-desktop").click(function() {
-                $(".js-profile-main").css('width','100vw');
-            });
-        </script>
-    </body>
-</html>
+            <div class="carousel-item">
+                <div class="d-block w-100 hero-image-t2">
+                    <img class="hero-background d-block w-100" src="/img/bgd2.jpg">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p class="text-white bg-dark">The first urban settlement was built here in the 3rd century 
+                            BC by the Celts. Not far from Belgrade, is Vinca, a settlement older than the Mesopotamia.</p>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="carousel-item">
+                <div class="d-block w-100 hero-image-t2">
+                    <img class="hero-background" src="/img/bgd3.jpg">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p class="text-white bg-dark">The original Slovene name of the city, “Beligrad”, which means “white city”, 
+                            was first used in 878, and the city became the capital of Serbia in 1405.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <section class="about-section text-center" id="about">
+        <div class="container about-t2">
+            <div class="row">
+                <div class="col-lg-8 mx-auto">
+                    <h2 class="text-dark mb-4">About</h2>
+                    <p class="text-dark-50">
+                        Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
+                        <a href="https://startbootstrap.com/template-overviews/grayscale/">the preview page</a>
+                        . The theme is open source, and you can use it for any purpose, personal or commercial.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
