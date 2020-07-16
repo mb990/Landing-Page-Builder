9c3e000386d9f26385c12d0c03a6fb990513d914
pavle.birac <pavle.birac@quantox.com>
2020-07-16 14:21:14 +0200

    Profile update v2, Template 2 added elements

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -230,7 +230,7 @@
 
     <div class="col-lg-3 col-md-4 col-6">
         <a class="d-block mb-4 h-100 pop">
-            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/pWkk7iiCoDM/400x300" alt="">
+            <img class="img-fluid img-thumbnail" style="width: 210px; max-height: 160px;" src="https://source.unsplash.com/pWkk7iiCoDM/400x300" alt="">
             </a>
     </div>
     <div class="col-lg-3 col-md-4 col-6">
--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -13,78 +13,132 @@
 
 </head>
 <body class="body-t2">
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-shrink" id="mainNav">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
         <div class="container">
-            <a class="navbar-brand js-scroll-trigger text-primary" href="#page-top">LOGO NAME</a>
+            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img class="header-logo" src="#"></a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <svg class="svg-inline--fa fa-bars fa-w-14" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg><!-- <i class="fas fa-bars"></i> -->
+            </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger text-white bg-dark" href="#about">About</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger text-white bg-dark" href="#projects">Projects</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger text-white bg-dark" href="#signup">Contact</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger text-light" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger text-light" href="#projects">Projects</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger text-light" href="#signup">Contact</a></li>
                 </ul>
             </div>
         </div>
     </nav>
-
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <div class="d-block w-100 hero-image-t2">
-                    <img class="hero-background" src="/img/bgd1.jpg">
-                    <div class="hero-text-t2 text-white">
-                        <h1>Travel to Belgrade</h1>
-                        <button class="btn btn-primary">Book weekend</button>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <div class="d-block w-100 hero-image-t2">
-                    <img class="hero-background d-block w-100" src="/img/bgd2.jpg">
-                    <div class="carousel-caption d-none d-md-block">
-                        <p class="text-white bg-dark">The first urban settlement was built here in the 3rd century 
-                            BC by the Celts. Not far from Belgrade, is Vinca, a settlement older than the Mesopotamia.</p>
-                    </div>
-                </div>
-                
-            </div>
-            <div class="carousel-item">
-                <div class="d-block w-100 hero-image-t2">
-                    <img class="hero-background" src="/img/bgd3.jpg">
-                    <div class="carousel-caption d-none d-md-block">
-                        <p class="text-white bg-dark">The original Slovene name of the city, “Beligrad”, which means “white city”, 
-                            was first used in 878, and the city became the capital of Serbia in 1405.</p>
-                    </div>
-                </div>
+    <section class="hero" id="page-top">
+        <div class="hero-image">
+            <img class="hero-background-t2" src="/img/bgd2.jpg">
+            <div class="hero-text-t2">
+                <h1 class="text-white">Explore Belgrade</h1>
+                <h6 class="text-light">Enjoy in one of oldest cities in Europe</h6>
+                <button class="btn btn-warning">Get your tour</button>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
+        <div>
+        </div>
+    </section>
 
-    <section class="about-section text-center" id="about">
-        <div class="container about-t2">
-            <div class="row">
-                <div class="col-lg-8 mx-auto">
-                    <h2 class="text-dark mb-4">About</h2>
-                    <p class="text-dark-50">
-                        Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
-                        <a href="https://startbootstrap.com/template-overviews/grayscale/">the preview page</a>
-                        . The theme is open source, and you can use it for any purpose, personal or commercial.
-                    </p>
+    <section class="about-section text-center content1-t2 container" id="about">
+        <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+            <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="/img/bgd3.jpg" alt=""></div>
+            <div class="col-xl-4 col-lg-5">
+                <div class="featured-text text-center text-lg-left" style="margin: 20px;">
+                    <h4>ABOUT</h4>
+                    <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+                    <a class="btn" style="background-color: rgb(103, 103, 238); color: white;" href="">GO HERE</a>
                 </div>
             </div>
         </div>
     </section>
+
+    <div class="general-content" id="projects">
+        <div class="bullets-div-t2">
+            <div class="content center">
+                <div class="col span1of3 mid-span3of4 small-span1of1 align-left">
+                    <p><strong style="font-size: 24px;">Clock-making industry</strong>, is even older than the world-famous Swiss one.
+                        The Serbs had their own clock at least 200 years before the Swiss did.</p>
+                </div>
+                <div class="col span1of3 mid-span3of4 small-span1of1 align-left">
+                    <p><strong style="font-size: 24px;">Hospitality</strong>,
+                        Serbs are the most hospitable nation – according to the international polls.</p>
+                </div>
+                <div class="col span1of3 mid-span3of4 small-span1of1 align-left">
+                    <p><strong style="font-size: 24px;">Nightlife</strong>, Lonely Planet named Belgrade as the City with the Best Nightlife in the World. 
+                        The city has more than 170 river clubs which are the mark of the city’s nightlife.
+                        No matter what kind of music you listen to, you will definitely find a river club that will suit your taste.</p>
+                </div>
+            </div>
+        </div>
+        <section>
+            <div>
+                <div>
+                    <div>
+                        <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 text-center" style="margin: 48px auto;">
+                            <h1>Did you now?</h1>
+                            <div class="mb32">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. m dolor sit amet, consectetur adipi.
+                                    Vivamus imperdiet rhoncus porta. Ut quis sem quis purus lobortis dictum. Tam pulvinar luctus sem, eget porta orci. 
+                                    Maecenas molestie dui id diam feugiat, eu tincidunt mauris aliquam. Duis commodo vitae ligula et int
+                                    Aliquam nec dignissim nisl. Vivamus cursus feugiat sapien, eget tincidunt leo ornare quis.</p>
+                            </div>
+                            <a class="btn btn-lg btn-filled btn-primary" href="#">MORE INFO</a>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+
+
+<!-- Section: Testimonials v.3 -->
+<section class="team-section text-center my-5">
+    <h2 class="h1-responsive font-weight-bold my-5">Testimonials</h2>
+    <div class="row text-center" style="max-width: 100%; padding: 20px;">
+        <!--Grid column-->
+        <div class="col-md-4 mb-md-0 mb-5" style="padding: 0px;">
+            <div class="testimonial">
+                <div class="col-md-6 avatar mx-auto">
+                    <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(1).jpg" class="rounded-circle z-depth-1 img-fluid">
+                </div>
+                <h4 class="font-weight-bold dark-grey-text mt-4">Anna Deynah</h4>
+                <h6 class="font-weight-bold blue-text my-3">Web Designer</h6>
+                <p class="font-weight-normal dark-grey-text">
+                    <i class="fa fa-quote-right"></i>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                    eos id officiis hic tenetur quae quaerat ad velit ab hic tenetur.</p>
+            </div>
+        </div>
+        <div class="col-md-4 mb-md-0 mb-5" style="padding: 0px;">
+            <div class="testimonial">
+                <div class="col-md-6 avatar mx-auto">
+                    <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(8).jpg" class="rounded-circle z-depth-1 img-fluid">
+                </div>
+                <h4 class="font-weight-bold dark-grey-text mt-4">John Doe</h4>
+                <h6 class="font-weight-bold blue-text my-3">Web Developer</h6>
+                <p class="font-weight-normal dark-grey-text">
+                    <i class="fa fa-quote-right"></i>Ut enim ad minima veniam, quis nostrum exercitationem ullam
+                    corporis suscipit laboriosam, nisi ut aliquid commodi.</p>
+            </div>
+        </div>
+        <div class="col-md-4 mb-md-0 mb-5" style="padding: 0px;">
+            <div class="testimonial">
+                <div class="col-md-6 avatar mx-auto">
+                    <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg" style="margin: auto;" class="rounded-circle img-fluid"/>
+                </div>
+                <h4 class="font-weight-bold dark-grey-text mt-4">Maria Kate</h4>
+                <h6 class="font-weight-bold blue-text my-3">Photographer</h6>
+                <p class="font-weight-normal dark-grey-text">
+                    <i class="fa fa-quote-right"></i>At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                    blanditiis praesentium voluptatum deleniti atque corrupti.</p>
+            </div>
+        </div>
+    </div>
+
+</section>
 
 </body>
