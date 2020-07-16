7668e51dc0e9927af0114a6e10691fac7a43ba87
Spasoje Gojic <spasoje.gojic@quantox.com>
2020-07-16 14:07:36 +0200

    Slight style change on gallery image item

--- resources/views/templates/template1/page_elements/gallery-content.blade.php
+++ resources/views/templates/template1/page_elements/gallery-content.blade.php
@@ -1,5 +1,5 @@
 <div class="col-lg-3 col-md-4 col-6">
     <a class="d-block mb-4 h-100 pop">
-        <img class="img-fluid img-thumbnail" src="{{$image_url}}" alt="">
+        <img class="img-fluid img-thumbnail" src="{{$image_url}}" alt="" style="width: 210px; max-height: 160px;">
     </a>
 </div>
