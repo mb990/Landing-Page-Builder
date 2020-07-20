a3393ecbbb761e9e8837db72a02f5cb4b370a82e
pavle.birac <pavle.birac@quantox.com>
2020-07-20 10:30:28 +0200

    Pricing benefits color, icon updates

--- resources/views/templates/template1/page_elements/pricing-single.blade.php
+++ resources/views/templates/template1/page_elements/pricing-single.blade.php
@@ -5,15 +5,15 @@
             <h6 class="card-price text-center price-tag js-month">{{$item->monthly_price}}<span class="period">/Monthly </span></h6>
             <h6 class="card-price text-center price-tag d-none js-year">{{$item->yearly_price}}<span class="period">/Annual @if($item->discount_amount) <h6>{{$item->discount_amount}}% off</h6> @endif</span></h6>
             <hr>
-            <ul class="fa-ul">
+            <ul class="fa-ul text-secondary">
 
                 @forelse($item->benefits as $benefit)
 
-                    <li><span class="fa-li"><i class="fas fa-check"></i></span>{{$benefit->description}}</li>
+                    <li><span class="fa-li"><i class="fa fa-check "></i></span>{{$benefit->description}}</li>
 
                 @empty
 
-                    <li><span class="fa-li"><i class="fas fa-check"></i></span>No benefits for this plan</li>
+                    <li><span class="fa-li"><i class="fa fa-exclamation-circle"></i></span>No benefits for this plan</li>
 
                 @endforelse
 
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -182,11 +182,11 @@
                         <h6 class="card-price text-center price-tag js-month">$0<span class="period">/month</span></h6>
                         <h6 class="card-price text-center price-tag d-none js-year">$10<span class="period">/year <h6>30%off</h6></span></h6>
                         <hr>
-                        <ul class="fa-ul">
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>One Project</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+                        <ul class="fa-ul text-secondary">
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>Single User</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>5GB Storage</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>One Project</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>Community Access</li>
                         </ul>
                         <a href="#" class="btn btn-block btn-primary btn-lg">Get Plan</a>
                     </div>
@@ -201,11 +201,11 @@
                         </h6>
                         <hr>
                         <ul class="fa-ul">
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Five Users</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>10GB Storage</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Five Projects</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Online support</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>Five Users</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>10GB Storage</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>Five Projects</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>Community Access</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>Online support</li>
                         </ul>
                         <a href="#" class="btn btn-block btn-primary btn-lg">Get Plan</a>
                     </div>
@@ -220,12 +220,12 @@
                         </h6>
                         <hr>
                         <ul class="fa-ul">
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Users</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Storage</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Projects</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Online support</li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span>24/7 skype call</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>Unlimited Users</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>Unlimited Storage</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>Unlimited Projects</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>Community Access</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>Online support</li>
+                            <li><span class="fa-li"><i class="fa fa-check"></i></span>24/7 skype call</li>
                         </ul>
                         <a href="#" class="btn btn-block btn-primary btn-lg">Get Plan</a>
                     </div>
