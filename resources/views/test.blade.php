f78d5af65e6d9aba8dedcb4cb3f9c22f2a36f7ee
pavle.birac <pavle.birac@quantox.com>
2020-07-22 16:31:45 +0200

    Template 2 pricing box added

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -319,16 +319,6 @@
             <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/p2TQ-3Bh3Oo/400x300" alt="">
             </a>
     </div>
-    <div class="col-lg-3 col-md-4 col-6">
-        <a class="d-block mb-4 h-100 pop">
-            <img src="/img/video-logo.png"  alt="Audition" class="img-fluid mx-auto d-block">
-            <span style="display: none;">
-                <video controls style="max-width: 210px; max-height: 160px;">
-                    <source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
-                </video>
-            </span>
-            </a>
-    </div>
     </div>
     <div class="modal fade" id="imagemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog">
--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -311,74 +311,89 @@
         </div>
     </div>
 </div>
-<div class='wrapper'>
-    <div class='package'>
-      <div class='name'>Limited</div>
-      <div class='price'>€2</div>
-      <div class='trial'>Available for a month</div>
-      <hr>
-      <ul>
-        <li>
-          <strong>8</strong>
-          team members
-        </li>
-        <li>
-          <strong>6</strong>
-          team playlists
-        </li>
-        <li>
-          <strong>Unlimited</strong>
-          public playlists
-        </li>
-      </ul>
+<section class="pricing py-5">
+    <div class="container">
+        <!-- switcher -->
+        <div style="text-align: center;" class="custom-control custom-switch">
+            <input type="checkbox" class="custom-control-input" id="customSwitches">
+            <label class="custom-control-label" for="customSwitches">Monthly / Annual</label>
+        </div>
+        <br>
+        <div class="row" style="justify-content: center;">
+        <div class='package'>
+            <div class='name'>Limited</div>
+            <div class='price text-secondary'>€2</div>
+            <div class='trial text-secondary'>Available for a month</div>
+            <hr>
+            <ul>
+                <li>
+                    <strong>8</strong>
+                    team members
+                </li>
+                <li>
+                    <strong>6</strong>
+                    team playlists
+                </li>
+                <li>
+                    <strong>Unlimited</strong>
+                    public playlists
+                </li>
+                </ul>
+                <a class="btn btn-warning" type="button">Get this</a>
+
+        </div>
+        <div class='package brilliant'>
+            <div class='name'>Brilliant</div>
+            <div class='price'>€5</div>
+            <div class='trial'>Free 30 day trial</div>
+            <hr>
+            <ul>
+            <li>
+                <strong>Unlimited</strong>
+                team members
+            </li>
+            <li>
+                <strong>Unlimited</strong>
+                team playlists
+            </li>
+            <li>
+                <strong>Unlimited</strong>
+                public playlists
+            </li>
+            <li>
+                Team analytics
+            </li>
+            <li>
+                Send files
+            </li>
+            </ul>
+            <a class="btn btn-warning" type="button">Get this</a>
+
+        </div>
+            <div class='package'>
+                <div class='name'>Basic</div>
+                <div class='price'>€0</div>
+                <div class='trial'>Totally free</div>
+                <hr>
+                <ul>
+                <li>
+                    <strong>5</strong>
+                    team members
+                </li>
+                <li>
+                    <strong>3</strong>
+                    team playlists
+                </li>
+                <li>
+                    <strong>Unlimited</strong>
+                    public playlists
+                </li>
+                </ul>
+                <a class="btn btn-warning" type="button">Get this</a>
+            </div>
+        </div>
     </div>
-    <div class='package brilliant'>
-      <div class='name'>Brilliant</div>
-      <div class='price'>€5</div>
-      <div class='trial'>Free 30 day trial</div>
-      <hr>
-      <ul>
-        <li>
-          <strong>Unlimited</strong>
-          team members
-        </li>
-        <li>
-          <strong>Unlimited</strong>
-          team playlists
-        </li>
-        <li>
-          <strong>Unlimited</strong>
-          public playlists
-        </li>
-        <li>
-          Team analytics
-        </li>
-        <li>
-          Send files
-        </li>
-      </ul>
-    </div>
-    <div class='package'>
-      <div class='name'>Basic</div>
-      <div class='price'>€0</div>
-      <div class='trial'>Totally free</div>
-      <hr>
-      <ul>
-        <li>
-          <strong>5</strong>
-          team members
-        </li>
-        <li>
-          <strong>3</strong>
-          team playlists
-        </li>
-        <li>
-          <strong>Unlimited</strong>
-          public playlists
-        </li>
-      </ul>
-    </div>
-  </div>
+</section>
 <!-- FOOTER -->
 <footer class="page-footer font-small cyan darken-3">
     <div class="container">
