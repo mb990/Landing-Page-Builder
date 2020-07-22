967b1b3fcf4355fa65af7e8463c612d249e02fc5
pavle.birac <pavle.birac@quantox.com>
2020-07-22 14:22:57 +0200

    Template 2 gallery modal updated v2.1

--- resources/views/templates/template1/page_elements/gallery.blade.php
+++ resources/views/templates/template1/page_elements/gallery.blade.php
@@ -19,7 +19,9 @@
             <div class="modal-content">
                 <div class="modal-body">
                     <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                <img src="" class="imagepreview" style="width: 100%;" >
+                    <img src="" class="imagepreview" style="width: 100%;" >
+                    <video class="videopreview" controls style="width: 100%;">
+                        <source src="" class="videopreview" style="width: 100%;" type="video/mp4">
                 </div>
             </div>
         </div>
--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -311,6 +311,74 @@
         </div>
     </div>
 </div>
+<div class='wrapper'>
+    <div class='package'>
+      <div class='name'>Limited</div>
+      <div class='price'>€2</div>
+      <div class='trial'>Available for a month</div>
+      <hr>
+      <ul>
+        <li>
+          <strong>8</strong>
+          team members
+        </li>
+        <li>
+          <strong>6</strong>
+          team playlists
+        </li>
+        <li>
+          <strong>Unlimited</strong>
+          public playlists
+        </li>
+      </ul>
+    </div>
+    <div class='package brilliant'>
+      <div class='name'>Brilliant</div>
+      <div class='price'>€5</div>
+      <div class='trial'>Free 30 day trial</div>
+      <hr>
+      <ul>
+        <li>
+          <strong>Unlimited</strong>
+          team members
+        </li>
+        <li>
+          <strong>Unlimited</strong>
+          team playlists
+        </li>
+        <li>
+          <strong>Unlimited</strong>
+          public playlists
+        </li>
+        <li>
+          Team analytics
+        </li>
+        <li>
+          Send files
+        </li>
+      </ul>
+    </div>
+    <div class='package'>
+      <div class='name'>Basic</div>
+      <div class='price'>€0</div>
+      <div class='trial'>Totally free</div>
+      <hr>
+      <ul>
+        <li>
+          <strong>5</strong>
+          team members
+        </li>
+        <li>
+          <strong>3</strong>
+          team playlists
+        </li>
+        <li>
+          <strong>Unlimited</strong>
+          public playlists
+        </li>
+      </ul>
+    </div>
+  </div>
 <!-- FOOTER -->
 <footer class="page-footer font-small cyan darken-3">
     <div class="container">
