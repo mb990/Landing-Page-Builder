ee3f9516ed4dc4f189cec7affead5cd1b932b3b8
pavle.birac <pavle.birac@quantox.com>
2020-07-24 10:42:04 +0200

    Profile testimonial modal update v.1.4

--- resources/views/profile/modals/testimonials-modal.blade.php
+++ resources/views/profile/modals/testimonials-modal.blade.php
@@ -1,20 +1,62 @@
-<span class="js-testimonial" style="display:flex; flex-direction:column;">
+<hr>
+<span>Select number of testimonials</span>
+<select class="js-testimonial-number">
+        <option value="0" selected>0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+    </select>
+<hr>
+<span class="js-testimonial js-testimonial-1 d-none" style="display:flex; flex-direction:column;">
     <input type="file" id="js-testimonial-image-1" required>
     <input type="text" id="customer_name-1" placeholder="Enter user name">
     <input type="text" id="testimonial_title-1" placeholder="Review title">
     <input type="text" id="testimonial_text-1" placeholder="Enter user review">
 </span>
     <br>
-<span class="js-testimonial" style="display:flex; flex-direction:column;">
+<span class="js-testimonial js-testimonial-2 d-none" style="display:flex; flex-direction:column;">
     <input type="file" id="js-testimonial-image-2" required>
     <input type="text" id="customer_name-2" placeholder="Enter user name">
     <input type="text" id="testimonial_title-2" placeholder="Review title">
     <input type="text" id="testimonial_text-2" placeholder="Enter user review">
 </span>
     <br>
-<span class="js-testimonial" style="display:flex; flex-direction:column;">
+<span class="js-testimonial js-testimonial-3 d-none" style="display:flex; flex-direction:column;">
     <input type="file" id="js-testimonial-image-3" required>
     <input type="text" id="customer_name-3" placeholder="Enter user name">
     <input type="text" id="testimonial_title-3" placeholder="Review title">
     <input type="text" id="testimonial_text-3" placeholder="Enter user review">
 </span>
+    <br>
+<span class="js-testimonial js-testimonial-4 d-none" style="display:flex; flex-direction:column;">
+    <input type="file" id="js-testimonial-image-4" required>
+    <input type="text" id="customer_name-4" placeholder="Enter user name">
+    <input type="text" id="testimonial_title-4" placeholder="Review title">
+    <input type="text" id="testimonial_text-4" placeholder="Enter user review">
+</span>
+<script>
+$(document).ready(function(){
+    $('.js-testimonial-number').on('change', function(){
+        if($(this).val()==1){
+            $('.js-testimonial').addClass('d-none')
+            $('.js-testimonial-1').removeClass('d-none')
+        }
+        if($(this).val()==2){
+            $('.js-top-menu-link').addClass('d-none')
+            $('.js-testimonial-1, .js-testimonial-2').removeClass('d-none')
+        }
+        if($(this).val()==3){
+            $('.js-top-menu-link').addClass('d-none')
+            $('.js-testimonial-1, .js-testimonial-2, .js-testimonial-3').removeClass('d-none')
+        }
+        if($(this).val()==4){
+            $('.js-top-menu-link').addClass('d-none')
+            $('.js-testimonial-1, .js-testimonial-2, .js-testimonial-3, .js-testimonial-4').removeClass('d-none')
+        }
+        if($(this).val()==0){
+            $('.js-testimonial').addClass('d-none')
+        }
+    })
+})
+</script>
--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -13,31 +13,28 @@
     <body>
         <header class="header-main-profile">
             <div style="flex-grow: 1;"><img style="width: 100px;transform: translateY(-25%);" src="{{ asset('img/logo.png') }}"></div>
-            <!-- <div style="flex-grow: 1;">
+            <div style="flex-grow: 1;">
                 <button class="js-mobile">Mobile</button>
                 <button class="js-desktop">Desktop</button>
-            </div> -->
+            </div>
             <div class="js-link">
                 <button class="head-link" id="js-info">Log out</button>
             </div>
         </header>
-        <main><!--style="display: flex;" -->
-            
-            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">Add element</button>
-
-            <!-- <aside class="profile-aside">
+        <main style="display: flex;">
+            <aside class="profile-aside">
                 <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                     <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">New project</a>
                     <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
                     <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
                     <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">All project</a>
                 </div>
-            </aside> -->
+            </aside>
             <div class="profile-main js-profile-main">
                 <div class="tab-content" id="v-pills-tabContent">
                     <!-- TAB1 -->
                     <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-                        <!-- <div class="card-columns">
+                        <div class="card-columns">
                             <div class="card">
                                 <img class="card-img-top" src="https://source.unsplash.com/2gYsZUmockw/100px160/" alt="Card image cap">
                                 <div class="card-body">
@@ -45,7 +42,7 @@
                                     <a href="#" class="btn btn-primary">Use this template</a>
                                 </div>
                             </div>
-                        </div> -->
+                        </div>
                     </div>
                     <!-- TAB2 -->
                     <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
@@ -69,99 +66,12 @@
                 </div>
             </div>
         </main>
-        <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-            <span>Select element</span>
-            <select id="select">
-                <option value="" selected disabled>Choose element</option>
-                <option value="1">Top menu</option>
-                <option value="2">Hero section</option>
-                <option value="3">General content 1</option>
-                <option value="4">General content 2</option>
-                <option value="5">General content 3</option>
-                <option value="6">Pricing</option>
-                <option value="7">Testimonials</option>
-                <option value="8">Gallery</option>
-                <option value="9">Subscribe</option>
-                <option value="10">Footer</option>
-            </select>
-
-            <div class="js-modal-content js-content-1">
-                @include('profile.modals.top-menu-modal')
-            </div>
-
-            <div class="js-modal-content js-content-2">
-                @include('profile.modals.hero-modal')
-            </div>
-
-            <div class="js-modal-content js-content-3">
-                @include('profile.modals.general1-modal')
-            </div>
-
-            <div class="js-modal-content js-content-4">
-                @include('profile.modals.general2-modal')
-            </div>
-
-            <div class="js-modal-content js-content-5">
-                @include('profile.modals.general3-modal')
-            </div>
-
-            <div class="js-modal-content js-content-6">
-                @include('profile.modals.pricing-modal')
-            </div>
-
-            <div class="js-modal-content js-content-7">
-                @include('profile.modals.testimonials-modal')
-            </div>
-
-            <div class="js-modal-content js-content-8">
-                @include('profile.modals.gallery-modal')
-            </div>
-
-            <div class="js-modal-content js-content-9">
-                @include('profile.modals.subscribe-modal')
-            </div>
-
-            <div class="js-modal-content js-content-10">
-                @include('profile.modals.footer-modal')
-            </div>
-
-
-
-
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-success js-add-element-btn">Add element</button>
-            </div>
-        </div>
-    </div>
-</div>
         <script>
             $(".js-mobile").click(function() {
                 $(".js-profile-main").css('width','425px');
             });
             $(".js-desktop").click(function() {
                 $(".js-profile-main").css('width','100vw');
-            });
-            $('.js-modal-content').hide();
-            $('#select').on("change",function () {
-                $('.js-modal-content').hide();
-                $('.js-content-'+$(this).val()).show();
-            });
-            $("#exampleModal").on("hidden.bs.modal", function () {
-                $('.js-modal-content').hide();
-                var select = $("#select");
-                select.val(select[0].options[0].value);
             });
         </script>
     </body>
