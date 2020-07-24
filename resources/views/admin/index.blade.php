e85511fd0e54ddd22c4dc98163e9ff093813ffee
pavle.birac <pavle.birac@quantox.com>
2020-07-24 15:23:33 +0200

    Admin update v1.3

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,9 +7,18 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="{{ asset('css/master.css') }}">
 
     </head>
     <body>
-
+    <header class="header-main">
+        <div style="flex-grow: 1;"><img style="width: 100px;transform: translateY(-25%);" src="{{ asset('img/logo.png') }}"></div>
+        <div class="js-link">
+            <button class="head-link" id="js-about">About</button>
+            <button class="head-link" id="js-info">Info</button>
+        </div>
+    </header>
+    <main class="main-content">
+    </main>
     </body>
 </html>
