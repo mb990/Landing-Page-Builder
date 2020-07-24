ebe05e639d8597da670152599f731d592e91c031
pavle.birac <pavle.birac@quantox.com>
2020-07-24 12:54:40 +0200

    Profile pricing modal update v.1.4

--- resources/views/profile/modals/pricing-modal.blade.php
+++ resources/views/profile/modals/pricing-modal.blade.php
@@ -1,7 +1,17 @@
-<span>
+<hr>
+<select class="js-pricing-number">
+    <option value="0" selected>0</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+</select>
+<span>Select number of pricing plans</span>
+<br>
+<hr>
+<span class="js-pricing js-pricing-1 d-none">
     <div class="input-group mb-3">
         <div class="input-group-append">
-            <span class="input-group-text text-primary" id="basic-addon2">Plan name</span>
+            <span class="input-group-text text-success" id="basic-addon2">Plan name</span>
         </div>
         <input type="text" class="form-control js-pricing-plan plan-1" placeholder="Enter plan name" aria-label="Recipient's username" aria-describedby="basic-addon2">
     </div>
@@ -20,21 +30,37 @@
         </div>
     </div>
     <span>
+        <hr>
+        <select class="js-benefits-1-number">
+            <option value="0" selected>0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+        </select>
+        <span>Select number of benefits</span>
+        <br>
         <div class="input-group">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="">Plan benefits</span>
-            </div>
-            <input type="text" class="form-control js-plan-benefit-1 benefit-1-1" placeholder="Benefit">
-            <input type="text" class="form-control js-plan-benefit-1 benefit-1-2" placeholder="Benefit">
-            <input type="text" class="form-control js-plan-benefit-1 benefit-1-3" placeholder="Benefit">
+            <input type="text" class="form-control js-plan-benefit-1 benefit-1-1 d-none" placeholder="Enter benefit">
+            <input type="text" class="form-control js-plan-benefit-1 benefit-1-2 d-none" placeholder="Enter benefit">
+        </div>
+        <div class="input-group">
+            <input type="text" class="form-control js-plan-benefit-1 benefit-1-3 d-none" placeholder="Enter benefit">
+            <input type="text" class="form-control js-plan-benefit-1 benefit-1-4 d-none" placeholder="Enter benefit">
+        </div>
+        <div class="input-group">
+            <input type="text" class="form-control js-plan-benefit-1 benefit-1-5 d-none" placeholder="Enter benefit">
+            <input type="text" class="form-control js-plan-benefit-1 benefit-1-6 d-none" placeholder="Enter benefit">
         </div>
     </span>
 </span>
-<br>
-<span>
+<hr>
+<span class="js-pricing js-pricing-2 d-none">
     <div class="input-group mb-3">
         <div class="input-group-append">
-            <span class="input-group-text text-primary" id="basic-addon2">Second plan name</span>
+            <span class="input-group-text text-success" id="basic-addon2">Second plan name</span>
         </div>
         <input type="text" class="form-control js-pricing-plan plan-2" placeholder="Enter plan name" aria-label="Recipient's username" aria-describedby="basic-addon2">
     </div>
@@ -54,21 +80,37 @@
         </div>
     </div>
     <span>
+        <hr>
+        <select class="js-benefits-2-number">
+            <option value="0" selected>0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+        </select>
+        <span>Select number of benefits</span>
+        <br>
         <div class="input-group">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="">Plan benefits</span>
-            </div>
-            <input type="text" class="form-control js-plan-benefit-2 benefit-2-1" placeholder="Benefit">
-            <input type="text" class="form-control js-plan-benefit-2 benefit-2-2" placeholder="Benefit">
-            <input type="text" class="form-control js-plan-benefit-2 benefit-2-3" placeholder="Benefit">
+            <input type="text" class="form-control js-plan-benefit-2 benefit-2-1 d-none" placeholder="Enter benefit">
+            <input type="text" class="form-control js-plan-benefit-2 benefit-2-2 d-none" placeholder="Enter benefit">
+        </div>
+        <div class="input-group">
+            <input type="text" class="form-control js-plan-benefit-2 benefit-2-3 d-none" placeholder="Enter benefit">
+            <input type="text" class="form-control js-plan-benefit-2 benefit-2-4 d-none" placeholder="Enter benefit">
+        </div>
+        <div class="input-group">
+            <input type="text" class="form-control js-plan-benefit-2 benefit-2-5 d-none" placeholder="Enter benefit">
+            <input type="text" class="form-control js-plan-benefit-2 benefit-2-6 d-none" placeholder="Enter benefit">
         </div>
     </span>
 </span>
-<br>
-<span>
+<hr>
+<span class="js-pricing js-pricing-3 d-none">
     <div class="input-group mb-3">
         <div class="input-group-append">
-            <span class="input-group-text text-primary" id="basic-addon2">Third plan name</span>
+            <span class="input-group-text text-success" id="basic-addon2">Third plan name</span>
         </div>
         <input type="text" class="form-control js-pricing-plan plan-3" placeholder="Enter plan name" aria-label="Recipient's username" aria-describedby="basic-addon2">
     </div>
@@ -88,13 +130,95 @@
         </div>
     </div>
     <span>
+        <hr>
+        <select class="js-benefits-3-number">
+            <option value="0" selected>0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+        </select>
+        <span>Select number of benefits</span>
+        <br>
         <div class="input-group">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="">Plan benefits</span>
-            </div>
-            <input type="text" class="form-control js-plan-benefit-3 benefit-3-1" placeholder="Benefit">
-            <input type="text" class="form-control js-plan-benefit-3 benefit-3-2" placeholder="Benefit">
-            <input type="text" class="form-control js-plan-benefit-3 benefit-3-3" placeholder="Benefit">
+            <input type="text" class="form-control js-plan-benefit-3 benefit-3-1 d-none" placeholder="Benefit">
+            <input type="text" class="form-control js-plan-benefit-3 benefit-3-2 d-none" placeholder="Benefit">
+        </div>
+        <div class="input-group">
+            <input type="text" class="form-control js-plan-benefit-3 benefit-3-3 d-none" placeholder="Benefit">
+            <input type="text" class="form-control js-plan-benefit-3 benefit-3-4 d-none" placeholder="Benefit">
+        </div>
+        <div class="input-group">
+            <input type="text" class="form-control js-plan-benefit-3 benefit-3-5 d-none" placeholder="Benefit">
+            <input type="text" class="form-control js-plan-benefit-3 benefit-3-6 d-none" placeholder="Benefit">
         </div>
     </span>
 </span>
+<script>
+    $(document).ready(function(){
+        $('.js-pricing-number').on('change', function(){
+            if($(this).val()==1){
+                $('.js-pricing').addClass('d-none')
+                $('.js-pricing-1').removeClass('d-none')
+            }
+            if($(this).val()==2){
+                $('.js-pricing').addClass('d-none')
+                $('.js-pricing-1, .js-pricing-2').removeClass('d-none')
+            }
+            if($(this).val()==3){
+                $('.js-pricing').addClass('d-none')
+                $('.js-pricing-1, .js-pricing-2, .js-pricing-3').removeClass('d-none')
+            }
+            if($(this).val()==0){
+                $('.js-pricing').addClass('d-none')
+            }
+        })
+        $('.js-benefits-1-number').on('change', function(){
+            benefits = $(this).val();
+            $('.js-plan-benefit-1').addClass('d-none');
+            for (i = 0; i < 7; i++) {
+                if(i==benefits){
+                    console.log(benefits)
+                    for (y = 0; y < i+1; y++){
+                        $('.benefit-1-'+ y ).removeClass('d-none');
+                    }
+                }
+            }
+            if($(this).val()==0){
+                $('.js-plan-benefit-1').addClass('d-none')
+            }
+        })
+        $('.js-benefits-2-number').on('change', function(){
+            benefits = $(this).val();
+            $('.js-plan-benefit-2').addClass('d-none');
+            for (i = 0; i < 7; i++) {
+                if(i==benefits){
+                    console.log(benefits)
+                    for (y = 0; y < i+1; y++){
+                        $('.benefit-2-'+ y ).removeClass('d-none');
+                    }
+                }
+            }
+            if($(this).val()==0){
+                $('.js-plan-benefit-2').addClass('d-none')
+            }
+        })
+        $('.js-benefits-3-number').on('change', function(){
+            benefits = $(this).val();
+            $('.js-plan-benefit-3').addClass('d-none');
+            for (i = 0; i < 7; i++) {
+                if(i==benefits){
+                    console.log(benefits)
+                    for (y = 0; y < i+1; y++){
+                        $('.benefit-3-'+ y ).removeClass('d-none');
+                    }
+                }
+            }
+            if($(this).val()==0){
+                $('.js-plan-benefit-3').addClass('d-none')
+            }
+        })
+    })
+    </script>
