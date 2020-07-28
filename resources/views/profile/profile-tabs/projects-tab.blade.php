c3f859af6ea79a7ff7a1bd7de510734b7ba1f5bd
spasoje.gojic <spasoje.gojic@quantox.com>
2020-07-28 11:31:35 +0200

    Basic add on user's projects

--- resources/views/profile/profile-tabs/projects-tab.blade.php
+++ resources/views/profile/profile-tabs/projects-tab.blade.php
@@ -2,15 +2,21 @@
 <div class="row">
     <div class="col-sm-6" style="max-width: 25vw; margin-bottom: 10px;">
 
+        @forelse(auth()->user()->projects as $project)
 
-        <div class="card">
-            <img class="card-img-top" src="https://source.unsplash.com/2gYsZUmockw/100px160/" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Template1</h5>
-                <a href="#" class="btn btn-success">Go somewhere</a>
+            <div class="card">
+                <img class="card-img-top" src="https://source.unsplash.com/2gYsZUmockw/100px160/" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{$project->name}}</h5>
+                    <a href="#" class="btn btn-success">Go somewhere</a>
+                </div>
             </div>
-        </div>
 
-        
+        @empty
+
+            <p>You dont have projects yet.</p>
+
+        @endforelse
+
     </div>
 </div>
