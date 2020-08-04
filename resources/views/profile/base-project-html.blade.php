f31204db410ca252eff9fea5d27b210b638d7919
spasoje.gojic <spasoje.gojic@quantox.com>
2020-08-04 10:25:12 +0200

    Small js fix

--- resources/views/profile/base-project-html.blade.php
+++ resources/views/profile/base-project-html.blade.php
@@ -9,7 +9,7 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
         <script src="{{asset('js/app.js')}}"></script>
         <link rel="stylesheet" href="{{ asset('css/page_elements1.css') }}">
-        <script src="{{asset('js/example.js')}}"></script>
+{{--        <script src="{{asset('js/example.js')}}"></script>--}}
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <script type="text/javascript">
             $.ajaxSetup({
