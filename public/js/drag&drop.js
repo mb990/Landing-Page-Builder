0d8ab8710c1ff8954b9c98fb797d7edbb12020b6
pavle.birac <pavle.birac@quantox.com>
2020-08-07 12:27:22 +0200

    Profile drag & drop update v2.

--- public/js/drag&drop.js
+++ public/js/drag&drop.js
@@ -1,15 +1,13 @@
-window.selectElement = function(){
+window.selectElement = function(event){
     if($(this).is('.btn')){
         return;
     }
     $(".js-added-element").addClass("moving-element");
     $(".js-added-element").children().addClass("d-none");
     $(".ui-state-default").prepend("<p class='js-moving'>Test</p>");
-    console.log("mouse down")
 };
 window.dropElement = function(){
     $(".js-added-element").removeClass("moving-element");
     $(".js-added-element").children().removeClass("d-none");
     $(".js-moving").remove();
-
 };
