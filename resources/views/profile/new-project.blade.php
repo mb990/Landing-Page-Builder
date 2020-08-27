65891ff246a6565852d738868ad07e0c85b329e7
pavle.birac <pavlegodfather@gmail.com>
2020-08-27 11:37:53 +0200

    Test push

--- resources/views/profile/new-project.blade.php
+++ resources/views/profile/new-project.blade.php
@@ -115,7 +115,7 @@
             </div>
         </div>
 
-        <!-- Modal -->
+        <!-- Modal  -->
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
